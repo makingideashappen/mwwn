f5c70b10f50e78ff4b5a5bd289b554b92858b9ec
mih <makingideashappenreal@gmail.com>
2023-11-27 12:32:43 +0100

    new file

---
+++ src/components/contact-detail-list.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import ContactForm from "./contact-form"
+import { Container, Heading, Flex, FlexList,Icon,Subhead,Kicker, LinkList, Section, Box, Button, Text } from "./ui"
+import { graphql } from "gatsby"
+
+
+function ContactDetail(props) {
+  return (
+    <Box center>
+      {/* {props.image && (
+        <Icon
+          alt={props.image.alt}
+          image={props.image.gatsbyImageData}
+          size="large"
+        />
+      )} */}
+      <Subhead>{props.heading}</Subhead>
+      <Text>{props.text}</Text>
+    </Box>
+  )
+}
+
+export default function ContactDetailList(props) {
+  console.log(props,"111")
+  return (
+    <Section padding={4} background="muted">
+      <Container>
+        <Flex gap={4} variant="responsive">
+          <Box width="half">
+            <FlexList>
+              <Box center paddingY={4}>
+                <Heading>
+                  {props.kicker && <Kicker>{props.kicker}</Kicker>}
+                  {props.heading}
+                </Heading>
+                {props.text && <Text>{props.text}</Text>}
+              </Box>
+              <FlexList gap={3} variant="center">
+                { props.content.map((contactDetail) => (
+                  <li key={contactDetail.id}>
+                    <ContactDetail {...contactDetail} />
+                  </li>
+                ))}
+              </FlexList>
+            </FlexList>
+          </Box>
+          <Box width="half">
+            <ContactForm />
+          </Box>
+        </Flex>
+      </Container>
+    </Section>
+
+  )
+}
+
+export const query = graphql`
+  fragment ContactDetailListContent on ContactDetailList {
+    id
+    kicker
+    heading
+    text
+    content {
+      id
+      heading
+      text
+    }
+  }
+`
